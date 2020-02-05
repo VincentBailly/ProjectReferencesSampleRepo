effbfdd96a59c2788919efbc5a5f2cf4e638ba60
Vincent <Vincent.Bailly@microsoft.com>
2020-02-05 10:38:54 +0100

    typo

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,4 @@
 import * as sdf from "../../shared/lib/index";
 import * as dsf from "../../shared/lib/shared";
-sdf.sharedVar.strings.webStrings;
+sdf.sharedVar.strings.webString;
 console.log(dsf)
